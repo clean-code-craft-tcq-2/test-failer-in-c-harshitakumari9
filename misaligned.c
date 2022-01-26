ca991d0673f6a980ac3e050a1a9fcb888362ca58
harshitakumari9 <91043927+harshitakumari9@users.noreply.github.com>
2022-01-26 23:25:53 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <assert.h>
 
+#define Color_Number 5U
+
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
@@ -13,9 +15,21 @@ int printColorMap() {
     return i * j;
 }
 
+//Test Code added for checking whether correct color pair is received
+void Test_Color_Pair(int pair_number, const char* major_color , const char* minor_color)
+{
+    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+       
+    assert( majorColor[pair_number / Color_Number] == major_color);
+    assert( minorColor[pair_number % Color_Number] == minor_color);
+     
+    printf("%d | %s | %s\n", pair_number, major_color, minor_color);
+}
+
 int main() {
     int result = printColorMap();
-    assert(result == 0);
+    Test_Color_Pair(18,"Yellow","Green");
     printf("All is well (maybe!)\n");
     return 0;
 }
