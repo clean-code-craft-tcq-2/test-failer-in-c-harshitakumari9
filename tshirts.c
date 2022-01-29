7abfe0b8144560a0424f33801d2ca0787a7b50ee
harshitakumari9 <91043927+harshitakumari9@users.noreply.github.com>
2022-01-29 13:34:21 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -4,9 +4,9 @@
 char size(int cms) 
 {
     char sizeName = '\0';
-    printf("%d",cms);
-    
-    if(cms <= 0 || cms > 128)
+  
+    //Assuming the Minimum Size is greeater than 0 and Maximum size is 100.
+    if(cms <= 0 || cms > 100)
     {
         printf("Invalid Size !");
     }
@@ -18,7 +18,7 @@ char size(int cms)
     {
         sizeName = 'M';
     } 
-    else if(cms > 42 && cms <= 127) 
+    else if(cms > 42 && cms <= 100) 
     {
         sizeName = 'L';
     }
@@ -31,7 +31,7 @@ int main()
     assert(size(38) == 'S');
     assert(size(42) == 'M');
     assert(size(-2) == '\0');
-    assert(size(125) == 'L');   
+    assert(size(100) == 'L');   
     assert(size(0) == '\0');
     assert(size(132) == '\0');
     
