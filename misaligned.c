c5b967a20c1a0f007804358fb95aa8f8bb5ad9cb
harshitakumari9 <91043927+harshitakumari9@users.noreply.github.com>
2022-01-30 21:49:17 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -9,7 +9,7 @@ int printColorMap() {
     int i = 0, j = 0;
     for(i = 1; i <= 5; i++) {
         for(j = 1; j <= 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[j]);
+            printf("%d | %s | %s\n", ((i * 5 + j)+1), majorColor[i], minorColor[j]);
         }
     }
     return i * j;
