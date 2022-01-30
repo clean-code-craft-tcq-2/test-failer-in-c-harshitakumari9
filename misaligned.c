d77b02e2fd0becf9a9411f335db860953d37cdaf
harshitakumari9 <91043927+harshitakumari9@users.noreply.github.com>
2022-01-30 22:50:08 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -3,16 +3,19 @@
 
 #define Color_Number 5U
 
-int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int i = 0, j = 0;
-    for(i = 1; i <= 5; i++) {
-        for(j = 1; j <= 5; j++) {
-            printf("%d | %s | %s\n", ((i * 5 + j)+1), majorColor[i], minorColor[j]);
-        }
-    }
-    return i * j;
+int printColorMap ()
+{
+  const char *majorColor[] = { "White", "Red", "Black", "Yellow", "Violet" };
+  const char *minorColor[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
+
+  for (int i = 0; i < 5; i++)
+  {
+      for (int j = 0; j < 5; j++)
+	{
+	    printf ("%d | %s | %s\n", ((i * 5 + j) + 1), majorColor[i], minorColor[j]);
+	}
+  }
+  return ((i * 5 + j) + 1);
 }
 
 //Test Code added for checking whether correct color pair is received
@@ -22,7 +25,7 @@ void Test_Color_Pair(int pair_number, const char* major_color , const char* mino
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
        
     assert( majorColor[pair_number / Color_Number] == major_color);
-    assert( minorColor[pair_number % Color_Number] == minor_color);
+    assert( minorColor[(pair_number % Color_Number)-1] == minor_color);
      
     printf("%d | %s | %s\n", pair_number, major_color, minor_color);
 }
